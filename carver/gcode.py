d9641c42b6b61a77b7ebaa27fb4d93cca3f794cb
Randy May <randy@MacBook-Pro.local>
2020-12-24 15:08:01 -0500

    round coordinates to 3 because some controller don't like coordinates with lots of decimal places

--- carver/gcode.py
+++ carver/gcode.py
@@ -40,37 +40,55 @@ def end():
 
 
 def goto_xy_fast(x, y):
+    x = round(x, 3)
+    y = round(y, 3)
     print('G0 X{0} Y{1}'.format(x, y), file=GCODE_OUTPUT_FILE)
 
 
 def goto_z_fast(z):
+    z = round(z, 3)
     print('G0 Z{0}'.format(z), file=GCODE_OUTPUT_FILE)
 
 
 def goto_fast(x, y, z):
+    x = round(x, 3)
+    y = round(y, 3)
+    z = round(z, 3)
     print('G0 X{0} Y{1} Z{2}'.format(x, y, z), file=GCODE_OUTPUT_FILE)
 
 
 def goto(x, y, z):
+    x = round(x, 3)
+    y = round(y, 3)
+    z = round(z, 3)
     print('G1 X{0} Y{1} Z{2}'.format(x, y, z), file=GCODE_OUTPUT_FILE)
 
 
 def goto_x(x):
+    x = round(x, 3)
     print('G1 X{0}'.format(x), file=GCODE_OUTPUT_FILE)
 
 
 def goto_y(y):
+    y = round(y, 3)
     print('G1 Y{0}'.format(y), file=GCODE_OUTPUT_FILE)
 
 
 def goto_z(z):
+    z = round(z, 3)
     print('G1 Z{0} F{1}'.format(z, GCODE_PLUNGE_SPEED), file=GCODE_OUTPUT_FILE)
     print('F{0}'.format(GCODE_FEED_RATE), file=GCODE_OUTPUT_FILE)
 
 
 def goto_xy(x, y):
+    x = round(x, 3)
+    y = round(y, 3)
     print('G1 X{0} Y{1}'.format(x, y), file=GCODE_OUTPUT_FILE)
 
 
 def clockwise_arc(to_x, to_y, center_offset_x, center_offset_y):
+    to_x = round(to_x, 3)
+    to_y = round(to_y, 3)
+    center_offset_x = round(center_offset_x, 3)
+    center_offset_y = round(center_offset_y, 3)
     print('G2 X{0} Y{1} I{2} J{3}'.format(to_x, to_y, center_offset_x, center_offset_y), file=GCODE_OUTPUT_FILE)
